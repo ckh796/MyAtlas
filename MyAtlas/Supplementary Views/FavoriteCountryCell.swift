defab26eec4735fd1af084becf11bbfb2d8095f1
Charbel Khalifeh Hachem <charbelkhalifehhachem@Charbels-MacBook-Air.local>
2025-07-31 22:33:15 +0300

    fixed glitch on main view when images load

--- MyAtlas/Supplementary Views/FavoriteCountryCell.swift
+++ MyAtlas/Supplementary Views/FavoriteCountryCell.swift
@@ -14,18 +14,27 @@ struct FavoriteCountryCell: View {
     let country: Country
     let onLongPress: () -> Void
 
+    @State private var imageLoaded = false
+
     var body: some View {
         VStack(spacing: 8) {
             WebImage(url: URL(string: country.flag))
                 .resizable()
+                .onSuccess { _, _, _ in
+                    DispatchQueue.main.async {
+                        imageLoaded = true
+                    }
+                }
                 .aspectRatio(4/2.7, contentMode: .fit)
                 .frame(maxWidth: .infinity)
                 .clipShape(RoundedRectangle(cornerRadius: 8))
 
-            Text(country.name)
-                .font(.system(size: 16))
-                .foregroundColor(.primary)
-                .multilineTextAlignment(.center)
+            if imageLoaded {
+                Text(country.name)
+                    .font(.system(size: 16))
+                    .foregroundColor(.primary)
+                    .multilineTextAlignment(.center)
+            }
         }
         .frame(maxWidth: .infinity)
         .contentShape(Rectangle())
